2baf4d3c59a8c80c9ddea880e25e6632a6ebc2bb
BrianBell422 <brianbell.uk@gmail.com>
2021-02-24 14:46:31 +0000

    Initial commit

---
+++ src/components/CharacterDetail.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div v-if='!character'>
+            <h3>Please pick a character</h3>
+        </div>
+        <div v-if='character'>
+            <h3>{{character.name}}</h3>
+            <p>Species: {{character.species}}</p>
+            <p>Status: {{character.status}}</p>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'character-detail',
+    props: ['character']
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ src/components/CharactersList.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+    <div>
+        <!-- <ul>
+            <list-item v-for="(character, index) in characters" :character="character" :key="index"></list-item>
+        </ul> -->
+        <label for="character_select">Select a Character:</label>
+        <select id="character_select" v-model="selectedCharacter">
+            <option disabled value="">Select a character</option>
+            <option v-for="(character, index) in characters" :key="index" :character="character">{{character.name}}</option>
+        </select>
+    </div>
+</template>
+
+<script>
+import ListItem from './ListItem.vue';
+
+export default {
+    name: 'characters-list',
+    props: ['characters'],
+    components: {
+        "list-item": ListItem
+    }
+}
+</script>
+
+<style>
+
+</style>
